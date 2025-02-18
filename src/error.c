2c59afa5f61c20c3ab689ceea04b8f8e3e799ab4
Hakan Kanburoğlu <145389964+hakankanburoglu@users.noreply.github.com>
2025-02-18 23:15:49 +0300

    Update error

--- src/error.c
+++ src/error.c
@@ -1,38 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdarg.h>
 #include <string.h>
 
 #include "error.h"
 
+static inline void print_error(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+}
+
 void error_hypex() {
-    fprintf(stderr, "hypex error\n");
+    print_error("hypex error\n");
     exit(EXIT_FAILURE);
 }
 
 void error_file(const char *file) {
-    fprintf(stderr, "no such file or directory: %s\n", file);
+    print_error("no such file or directory: %s\n", file);
     exit(EXIT_FAILURE);
 }
 
 void error_file_proc(const char *file) {
-    fprintf(stderr, "file processing error: %s\n", file);
+    print_error("file processing error: %s\n", file);
     exit(EXIT_FAILURE);
 }
 
 void error_file_read(const char *file) {
-    fprintf(stderr, "file could not be read completely: %s\n", file);
+    print_error("file could not be read completely: %s\n", file);
     exit(EXIT_FAILURE);
 }
 
-void error_char(const char *file, int line, int col, char c) {
-    if (file != NULL) fprintf(stderr, "%s:", file);
-    fprintf(stderr, "%d:%d invalid character: '%c' (0x%x)\n", line, col, c, c);
-    exit(EXIT_FAILURE);
+void error_char(const char *file, int line, int column, char c) {
+    if (file != NULL) print_error("%s:", file);
+    print_error("%d:%d error: invalid character: '%c' (0x%x)\n", line, column, c, c);
 }
 
-void error_number(const char *file, int line, int col, const char *value) {
-    if (file != NULL) fprintf(stderr, "%s:", file);
-    fprintf(stderr, "%d:%d invalid number literal: %s\n", line, col, value);
-    exit(EXIT_FAILURE);
+void error_number(const char *file, int line, int column, const char *value) {
+    if (file != NULL) print_error("%s:", file);
+    print_error("%d:%d error: invalid number literal: %s\n", line, column, value);
+}
+
+void error_indent(const char *file, int line, int column) {
+    if (file != NULL) print_error("%s:", file);
+    print_error("%d:%d error: invalid indentation\n", line, column);
 }
--- src/error.h
+++ src/error.h
@@ -9,8 +9,10 @@ void error_file_proc(const char *file);
 
 void error_file_read(const char *file);
 
-void error_char(const char *file, int line, int col, char c);
+void error_char(const char *file, int line, int column, char c);
 
-void error_number(const char *file, int line, int col, const char *value);
+void error_number(const char *file, int line, int column, const char *value);
+
+void error_indent(const char *file, int line, int column);
 
 #endif //ERROR_H
