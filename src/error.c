7ffd85e189018ecc50b7b9c8ef9607529938ddcd
Hakan Kanburoğlu <145389964+hakankanburoglu@users.noreply.github.com>
2025-02-20 00:58:32 +0300

    Update error.c

--- src/error.c
+++ src/error.c
@@ -6,44 +6,47 @@
 
 #include "error.h"
 
-static inline void print_error(const char *format, ...) {
+static inline void fatal_error(const char *format, ...) {
     va_list args;
     va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    exit(EXIT_FAILURE);
+}
+
+static inline void print_error(const char *file, int line, int column, const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    if (file != NULL) fprintf(stderr, "%s:", file);
+    fprintf(stderr, "%d:%d error: ", line, column);
     vfprintf(stderr, format, args);
     va_end(args);
 }
 
 void error_hypex() {
-    print_error("hypex error\n");
-    exit(EXIT_FAILURE);
+    fatal_error("hypex error\n");
 }
 
 void error_file(const char *file) {
-    print_error("no such file or directory: %s\n", file);
-    exit(EXIT_FAILURE);
+    fatal_error("no such file or directory: %s\n", file);
 }
 
 void error_file_proc(const char *file) {
-    print_error("file processing error: %s\n", file);
-    exit(EXIT_FAILURE);
+    fatal_error("file processing error: %s\n", file);
 }
 
 void error_file_read(const char *file) {
-    print_error("file could not be read completely: %s\n", file);
-    exit(EXIT_FAILURE);
+    fatal_error("file could not be read completely: %s\n", file);
 }
 
 void error_char(const char *file, int line, int column, char c) {
-    if (file != NULL) print_error("%s:", file);
-    print_error("%d:%d error: invalid character: '%c' (0x%x)\n", line, column, c, c);
+    print_error(file, line, column, "invalid character: '%c' (0x%x)\n", c, c);
 }
 
 void error_number(const char *file, int line, int column, const char *value) {
-    if (file != NULL) print_error("%s:", file);
-    print_error("%d:%d error: invalid number literal: %s\n", line, column, value);
+    print_error(file, line, column, "invalid number literal: %s\n", value);
 }
 
 void error_indent(const char *file, int line, int column) {
-    if (file != NULL) print_error("%s:", file);
-    print_error("%d:%d error: invalid indentation\n", line, column);
+    print_error(file, line, column, "invalid indentation\n");
 }
