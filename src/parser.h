8eb1888a311a2dc03cb841eeb408082d3fd210fc
Hakan Kanburoğlu <145389964+hakankanburoglu@users.noreply.github.com>
2025-02-26 01:21:02 +0300

    Create parser.h

---
+++ src/parser.h
@@ -0,0 +1,23 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <stddef.h>
+
+#include "token.h"
+#include "lexer.h"
+#include "node.h"
+
+typedef struct {
+    Token **tok_list;
+    size_t toklen;
+    Node *expr;
+    int offset;
+} Parser;
+
+Parser *init_parser(Lexer *lex);
+
+void parse(Parser *p);
+
+void free_parser(Parser *p);
+
+#endif //PARSER_H
