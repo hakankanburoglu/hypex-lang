15e593f7d05c506a616208e8e2c8385efb556c80
Hakan Kanburoğlu <145389964+hakankanburoglu@users.noreply.github.com>
2025-04-03 19:08:16 +0300

    Update errors

--- src/error.c
+++ src/error.c
@@ -1,12 +1,12 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <stdarg.h>
+#include <stdnoreturn.h>
 #include <string.h>
 
 #include "error.h"
 
-static inline void fatal_error(const char *format, ...) {
+noreturn void fatal_error(const char *format, ...) {
     va_list args;
     va_start(args, format);
     vfprintf(stderr, format, args);
@@ -14,7 +14,7 @@ static inline void fatal_error(const char *format, ...) {
     exit(EXIT_FAILURE);
 }
 
-static inline void print_error(const char *file, int line, int column, const char *format, ...) {
+void error(const char *file, int line, int column, const char *format, ...) {
     va_list args;
     va_start(args, format);
     if (file) fprintf(stderr, "%s:", file);
@@ -25,32 +25,4 @@ static inline void print_error(const char *file, int line, int column, const cha
 
 void error_hypex() {
     fatal_error("hypex error\n");
-}
-
-void error_file(const char *file) {
-    fatal_error("no such file or directory: %s\n", file);
-}
-
-void error_file_proc(const char *file) {
-    fatal_error("file processing error: %s\n", file);
-}
-
-void error_file_read(const char *file) {
-    fatal_error("file could not be read completely: %s\n", file);
-}
-
-void error_char(const char *file, int line, int column, char c) {
-    print_error(file, line, column, "invalid character: '%c' (0x%x)\n", c, c);
-}
-
-void error_number(const char *file, int line, int column, const char *value) {
-    print_error(file, line, column, "invalid number literal: %s\n", value);
-}
-
-void error_indent(const char *file, int line, int column) {
-    print_error(file, line, column, "invalid indentation\n");
-}
-
-void error_expect(const char *file, int line, int column, const char *expect) {
-    print_error(file, line, column, "expected %s\n", expect);   
 }
--- src/error.h
+++ src/error.h
@@ -1,20 +1,7 @@
-#ifndef ERROR_H
-#define ERROR_H
+#include <stdnoreturn.h>
+
+noreturn void fatal_error(const char *format, ...);
+
+void error(const char *file, int line, int column, const char *format, ...);
 
 void error_hypex();
-
-void error_file(const char *file);
-
-void error_file_proc(const char *file);
-
-void error_file_read(const char *file);
-
-void error_char(const char *file, int line, int column, char c);
-
-void error_number(const char *file, int line, int column, const char *value);
-
-void error_indent(const char *file, int line, int column);
-
-void error_expect(const char *file, int line, int column, const char *expect);
-
-#endif //ERROR_H
