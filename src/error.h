27d01c293666630ec9484d4ee43ea07da9a66335
Hakan Kanburoğlu <145389964+hakankanburoglu@users.noreply.github.com>
2025-06-15 17:04:29 +0300

    Add include guard to error.h

--- src/error.h
+++ src/error.h
@@ -1,3 +1,6 @@
+#ifndef ERROR_H
+#define ERROR_H
+
 #include <stdnoreturn.h>
 
 noreturn void fatal_error(const char *format, ...);
@@ -5,3 +8,5 @@ noreturn void fatal_error(const char *format, ...);
 void error(const char *file, int line, int column, const char *format, ...);
 
 void error_hypex();
+
+#endif //ERROR_H
