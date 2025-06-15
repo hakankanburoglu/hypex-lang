e7885dc44f645824a3fafe17ec8224346cd5ae48
Hakan Kanburoğlu <145389964+hakankanburoglu@users.noreply.github.com>
2025-06-15 17:41:54 +0300

    Update node.c

--- src/node.c
+++ src/node.c
@@ -1,66 +1,47 @@
-#include <stdlib.h>
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 
-#include "token.h"
-#include "node.h"
 #include "error.h"
+#include "node.h"
+#include "token.h"
 
 Node *make_node(int kind, Node *parent) {
     Node *node = (Node *)malloc(sizeof(Node));
     if (!node) error_hypex();
     node->kind = kind;
     node->parent = parent;
-    node->tok_list = NULL;
-    node->toklen = 0;
     return node; 
-}
-
-void consume_node(Node *node, Token *tok) {
-    node->tok_list = (Token **)realloc(node->tok_list, sizeof(Token) * (node->toklen + 1));
-    if (!node->tok_list) error_hypex();
-    node->tok_list[node->toklen++] = tok;
 }
 
 Type *make_type(int kind) {
     Type *type = (Type *)malloc(sizeof(Type));
+    if (!type) error_hypex();
     type->kind = kind;
     type->types = NULL;
     type->cap = 1;
     type->len = 0;
-    type->is_const = false;
-    type->is_static = false;
-    type->is_private = false;
-    type->is_protected = false;
-    type->is_by_ref = false;
+    type->flags = FLAG_NONE;
     return type;
 }
 
-void push_type(Type *type, Type *elm) {
-    if (type->len == type->cap) {
-        type->cap *= 2;
-        type->types = (Type **)realloc(type->types, sizeof(Type) * type->cap);
-        if (!type->types) error_hypex();
-    }
-    type->types[type->len++] = elm;
-}
-
-Type *copy_type(Type *type) {
-    Type *r = make_type(type->kind);
+Type *copy_type(const Type *type) {
+    Type *r = (Type *)malloc(sizeof(Type));
+    if (!type) error_hypex();
+    r->kind = type->kind;
+    r->types = type->types;
+    r->cap = type->cap;
+    r->len = type->len;
+    r->flags = type->flags;
     return r;
 }
 
-Node *copy_node(Node *node) {
+Node *copy_node(const Node *node) {
     Node *r = (Node *)malloc(sizeof(Node));
     if (!r) error_hypex();
     r->kind = node->kind;
     r->parent = node->parent;
-    r->tok_list = (Token **)malloc(sizeof(Token) * node->toklen);
-    if (!r->tok_list) error_hypex();
-    for (int i = 0; i < node->toklen; i++)
-        r->tok_list[i] = node->tok_list[i];
-    r->toklen = node->toklen;
     switch (node->kind) {
         case NODE_SOURCE: case NODE_BLOCK:
             r->data.block.body = (Node **)malloc(sizeof(Node) * node->data.block.cap);
@@ -69,6 +50,9 @@ Node *copy_node(Node *node) {
                 r->data.block.body[i] = node->data.block.body[i];
             r->data.block.cap = node->data.block.cap;
             r->data.block.len = node->data.block.len;
+            break;
+        case NODE_EXPR: case NODE_RET_STMT: case NODE_IF_STMT:
+            r->data.expr = node->data.expr;
             break;
         case NODE_UNARY_OP:
             r->data.unary_op.op = node->data.unary_op.op;
@@ -85,11 +69,8 @@ Node *copy_node(Node *node) {
             r->data.ternary_op.if_body = node->data.ternary_op.if_body;
             r->data.ternary_op.else_body = node->data.ternary_op.else_body;
             break;
-        case NODE_LITERAL:
-            r->data.literal.value = node->data.literal.value;
-            break;
-        case NODE_IDENT:
-            r->data.ident.value = node->data.ident.value;
+        case NODE_LITERAL: case NODE_IDENT:
+            r->data.value = node->data.value;
             break;
         case NODE_FUNC_DECL:
             r->data.func_decl.ident = node->data.func_decl.ident;
@@ -107,7 +88,7 @@ Node *copy_node(Node *node) {
             r->data.var_decl.type = copy_type(node->data.var_decl.type);
             r->data.var_decl.ident = node->data.var_decl.ident;
             break;
-        case NODE_CALL_EXPR:
+        case NODE_CALL_EXPR: case NODE_ARG_DECL:
             r->data.call_expr.callee = (char *)malloc(sizeof(char) * (node->data.call_expr.len + 1));
             if (!r->data.call_expr.callee) error_hypex();
             strcpy(r->data.call_expr.callee, node->data.call_expr.callee);
