709240c4fa219d5938b1a488153ed80881d3a466
Hakan Kanburoğlu <145389964+hakankanburoglu@users.noreply.github.com>
2026-01-04 00:40:36 +0300

    Refactor file_read function

--- src/parse/file_reader.c
+++ src/parse/file_reader.c
@@ -10,31 +10,18 @@ void file_read(Lexer *lex) {
     FILE *file = fopen(lex->file, "r");
     if (!file) fatal_error("no such file or directory: %s", lex->file);
     fseek(file, 0, SEEK_END);
-    size_t filelen = ftell(file);
-    if (filelen == -1L) {
+    long filelen = ftell(file);
+    if (filelen < 0) {
         fclose(file);
         fatal_error("file processing error: %s", lex->file);
     }
     rewind(file);
-    lex->input = (char *)realloc(lex->input, sizeof(char) * (filelen + 1));
-    if (!lex->input) {
-        fclose(file);
-        error_hypex();
-    }
-    size_t readlen = fread(lex->input, 1, filelen, file);
-    lex->cur = lex->input[0];
-    if (readlen < filelen) {
-        if (feof(file)) {
-            lex->input[readlen] = '\0';
-            lex->inputlen = readlen;
-        } else {
-            fclose(file);
-            fatal_error("file could not be read completely: %s", lex->file);
-        }
-        fclose(file);
-        return;
-    }
-    lex->input[filelen] = '\0';
+    char *buf = malloc(sizeof(char) * (filelen + 1));
+    if (!buf) error_hypex();
+    size_t readlen = fread(buf, 1, filelen, file);
     fclose(file);
+    buf[readlen] = '\0';
+    lex->input = buf;
     lex->inputlen = readlen;
+    lex->cur = lex->input[0];
 }
